8bc8f7a9902fb9b2d9b48f2795fbe1464a920957
HarukiOkamoto <alice.159357@icloud.com>
2024-01-17 13:04:08 +0900

    a

--- promise2/main.js
+++ promise2/main.js
@@ -28,4 +28,56 @@ promise.finally((value) => {
     console.log('fainaly', value);
 });
 
+//Promiseチェーンとは
+//then,catch,finallyの後ろには、いくつでも自由にthen,catch,finallyをつけることができる。
+new Promise((resolve) => resolve('1'))
+    .then((value) => {
+        console.log(value);
+        //returnした値を次のthenのvalueへ
+        return '2';
+    })
+    .then((value) => {
+        console.log(value);
+        //errorを受けると、catchが実行される
+        throw new Error('3' + '：errorメッセージ');
+    })
+    .catch((error) => {
+        console.log(error.message);
+        throw new Error('4' + '：errorメッセージ');
+    })
+    .catch((error) => {
+        console.log(error.message);
+        return '5';
+    })
+    .then((value) => {
+        console.log(value);
+        return '6';
+    })
+    //スルーされる
+    .catch(() => {})
+    //値を受け取る
+    .then((value) => {
+        console.log(value);
+        throw new Error('7');
+    })
+    .catch((error) => {
+        console.log(error.message);
+        return '8';
+    })
+    .catch(() => console.log('skip'))
+    .finally((value) => {
+        //finallyは値を受け取れないよね
+        console.log(value + '：順番が来たら実行');
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 1000);
+        });
+    })
+    //setTimeoutが終わるまで待たされる
+    //新しいPromiseがresolveになるまで
+    .then((value) => {
+        console.log(value);
+    });
+
 console.log(promise);

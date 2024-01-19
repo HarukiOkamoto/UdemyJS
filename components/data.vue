8534ce8490a69e0d60979d5fbaf8911946ac2e0a
HarukiOkamoto <alice.159357@icloud.com>
2024-01-19 23:06:57 +0900

    mock fin

--- components/data.vue
+++ components/data.vue
@@ -4,8 +4,6 @@ import { profile } from '@/types/mock.js';
 
 <template>
     <div>
-        <h1>{{ profile.name }}</h1>
-        <h1>{{ profile.color.red }}</h1>
-        <h1>{{ profile.color.green.color1 }}</h1>
+        <h1>{{ profile[0] }}</h1>
     </div>
 </template>
--- types/mock.ts
+++ types/mock.ts
@@ -1,29 +1,44 @@
+export type idType = {
+    [id: string]: mockData;
+};
+
 export type mockData = {
-    id: number;
     name: string;
     color: colorType;
 };
 
 export type colorType = {
     red: number;
-    green: colorType2;
-    blue: string;
+    green: needGreen;
+    blue: number;
 };
 
-export type colorType2 = {
+export type needGreen = {
     color1: string;
     color2: string;
 };
 
-export const profile: mockData = {
-    id: 1,
-    name: 'Haruki',
-    color: {
-        red: 255,
-        green: {
-            color1: 'yellow',
-            color2: 'blue',
+export const profile: idType = {
+    '1': {
+        name: 'Haruki',
+        color: {
+            red: 255,
+            green: {
+                color1: 'yellow',
+                color2: 'blue',
+            },
+            blue: 255,
         },
-        blue: 255,
+    },
+    '2': {
+        name: 'Haruki',
+        color: {
+            red: 255,
+            green: {
+                color1: 'yellow',
+                color2: 'blue',
+            },
+            blue: 255,
+        },
     },
 };
